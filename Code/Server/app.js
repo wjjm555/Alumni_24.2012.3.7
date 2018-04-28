ef1f0a366940ce256801b5f8cfd4c28ac8cfa4c7
wjjm555 <139040491@qq.com>
2018-04-28 17:54:47 +0800

    create chat server

--- Code/Server/app.js
+++ Code/Server/app.js
@@ -6,12 +6,15 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var chat = require('./routes/chat');
+var ejs = require('ejs');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', ejs.__express);
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,21 +24,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/chat', chat);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- Code/Server/routes/users.js
+++ Code/Server/routes/users.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/getName', function(req, res, next) {
+
+    // console.debug("req:");
+    // res.render('chat');
+    res.send('respond with a resource:' + req.query.user);
+});
+
+/* POST users listing. */
+router.post('/getUser', function(req, res, next) {
+
+    // console.debug("req:");
+
+    res.send('respond with a resource:' + JSON.stringify(req.body.user));
 });
 
 module.exports = router;
